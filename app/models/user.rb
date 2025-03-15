f60815ea8617f07418d01a13136fa70068e259ad
Sholee <woranan.sholee@gmail.com>
2025-03-15 17:51:42 +0700

    feat: add user registration and login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  attr_accessor :identifier
+
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :last_name, presence: true, length: { maximum: 50 }
+  validates :username, presence: true, length: { maximum: 50 }
+  validates :username, format: { with: /\A[a-zA-Z0-9_\.]*\z/, message: "can only contain letters, numbers, dots and underscores" }
+  validates :bio, length: { maximum: 500 }, allow_blank: true
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    identifier = conditions.delete(:identifier)
+    where(conditions).where([
+      "lower(username) = :value OR lower(email) = :value",
+      { value: identifier.downcase }
+    ]).first
+  end
 end
