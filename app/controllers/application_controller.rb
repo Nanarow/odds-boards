251cc2073430b2b0852e1aa675db3506ce6f1628
Sholee <woranan.sholee@gmail.com>
2025-03-16 21:59:35 +0700

    fix: add custom layout for public views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  layout :set_layout
+
+  private
+
+  def set_layout
+    user_signed_in? ? "application" : "public"
+  end
 end
