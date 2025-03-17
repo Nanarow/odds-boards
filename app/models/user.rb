436df30d52b28304820bbf6d3f43deb1644537de
Sholee <woranan.sholee@gmail.com>
2025-03-17 22:30:01 +0700

    fix: update registrations view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  # :confirmable, :lockable, :timeoutable, :trackable, :recoverable and :omniauthable 
+  devise :database_authenticatable, :registerable, :rememberable, :validatable
 
   attr_accessor :identifier
 
-  validates :first_name, presence: true, length: { maximum: 50 }
-  validates :last_name, presence: true, length: { maximum: 50 }
+  validates :first_name, length: { maximum: 50 }
+  validates :last_name, length: { maximum: 50 }
   validates :username, presence: true, length: { maximum: 50 }
   validates :username, format: { with: /\A[a-zA-Z0-9_\.]*\z/, message: "can only contain letters, numbers, dots and underscores" }
-  validates :bio, length: { maximum: 500 }, allow_blank: true
+  validates :bio, length: { maximum: 500 }
 
   def self.find_for_database_authentication(warden_conditions)
     conditions = warden_conditions.dup
