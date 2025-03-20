8b13fe59ddf07fb8ead6864c4d20c594c81c8d2c
Sholee <woranan.sholee@gmail.com>
2025-03-20 17:44:28 +0700

    fix: update public to auth layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,15 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   layout :set_layout
+  AUTH_PATHS = [ "/users/login", "/users/register" ]
 
   private
 
   def set_layout
-    user_signed_in? ? "application" : "public"
+    if AUTH_PATHS.include?(request.path)
+      "auth"
+    else
+      "application"
+    end
   end
 end
