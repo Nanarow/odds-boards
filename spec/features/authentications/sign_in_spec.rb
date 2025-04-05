3b86dfd97df276246c2ec38caf89dbbc6c983b05
Sholee <woranan.sholee@gmail.com>
2025-04-05 15:16:36 +0700

    fix: update signing in test

---
+++ spec/features/authentications/sign_in_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.feature "Signing in", type: :feature do
+    background do
+      create(:user)
+    end
+
+    scenario "Signing in with correct credentials" do
+      visit new_user_session_path
+
+      fill_in 'email', with: 'user1@example.com'
+      fill_in 'password', with: 'password'
+
+      click_on 'sign-in-button'
+
+      notification = find('notification')
+      expect(notification).to be_visible
+      expect(notification).to have_content 'Signed in successfully.'
+    end
+
+    scenario "Signing in as another user with wrong password" do
+      visit new_user_session_path
+
+      fill_in 'email', with: 'user1@example.com'
+      fill_in 'password', with: 'wrong-password'
+
+      click_on 'sign-in-button'
+
+      alert_notification = find('alert-notification')
+      expect(alert_notification).to be_visible
+      expect(alert_notification).to have_content 'Invalid username or email or password.'
+    end
+end
