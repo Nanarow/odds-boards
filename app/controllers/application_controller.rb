22c329d5e519ae7c8649c68084d632a4b0e1414b
Sholee <woranan.sholee@gmail.com>
2025-04-21 02:17:28 +0700

    fix: add "/users" path to AUTH_PATHS for authentication routing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   layout :set_layout
-  AUTH_PATHS = [ "/users/login", "/users/register" ]
+  AUTH_PATHS = [ "/users/login", "/users/register", "/users" ]
 
   private
 
